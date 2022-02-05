1e3509901f4f5b64cd196f702863bc22afd6d031
DESKTOP-9BT6J5O\CarlosLoura <cap.loura@gmail.com>
2022-02-05 14:57:52 +0100

    fixed name of test file

--- src/mongodb/functions.ts
+++ src/mongodb/functions.ts
@@ -1,75 +1,80 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
 const mongoDbConnection = async () => {
-    const username = process.env.MONGODB_USERNAME || '';
-    const password = process.env.MONGODB_PASSWORD || '';
+  const username = process.env.MONGODB_USERNAME || "";
+  const password = process.env.MONGODB_PASSWORD || "";
 
-    const mongoDbUrl = `mongodb+srv://${username}:${password}@challenge-xzwqd.mongodb.net/getircase-study?retryWrites=true`;
+  const mongoDbUrl = `mongodb+srv://${username}:${password}@challenge-xzwqd.mongodb.net/getircase-study?retryWrites=true`;
 
-    const client = new MongoClient(mongoDbUrl);
-    return await client.connect();
-}
+  const client = new MongoClient(mongoDbUrl);
+  return await client.connect();
+};
 
 const mongoDbCloseConnection = async (client: any) => {
-    await client.close();
-}
+  await client.close();
+};
 
-export const mongoDbQueryDataByDateAndTotalCount = async(startDate: Date, endDate: Date, maxCount: number, minCount: number) => {
-    const dbName = process.env.MONGODB_DATABASE || '';
-    const collectionName = process.env.MONGODB_COLLECTION || "";
+export const mongoDbQueryDataByDateAndTotalCount = async (
+  startDate: Date,
+  endDate: Date,
+  maxCount: number,
+  minCount: number
+) => {
+  const dbName = process.env.MONGODB_DATABASE || "";
+  const collectionName = process.env.MONGODB_COLLECTION || "";
 
-    const client = await mongoDbConnection();
+  const client = await mongoDbConnection();
 
-    try {
+  try {
+    console.log(
+      `Fetching data between ${startDate} and ${endDate}, with count between ${minCount} and ${maxCount}`
+    );
 
-        console.log(`Fetching data between ${startDate} and ${endDate}, with count between ${minCount} and ${maxCount}`);
+    const agg = [
+      {
+        $match: {
+          $and: [
+            { createdAt: { $lt: new Date(endDate) } },
+            { createdAt: { $gt: new Date(startDate) } },
+          ],
+        },
+      },
+      {
+        $project: {
+          key: 1,
+          createdAt: 1,
+          totalCount: {
+            $sum: "$counts",
+          },
+        },
+      },
+      {
+        $match: {
+          $and: [
+            { totalCount: { $gt: minCount } },
+            { totalCount: { $lt: maxCount } },
+          ],
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          key: 1,
+          createdAt: 1,
+          totalCount: 1,
+        },
+      },
+    ];
 
-        const agg = [
-            { 
-                $match: { 
-                    $and: [ 
-                        { createdAt: { $lt: new Date(endDate) } }, 
-                        { createdAt: { $gt: new Date(startDate) } }
-                    ] 
-                } 
-            },
-            { 
-                $project: { 
-                    "key": 1, 
-                    "createdAt": 1, 
-                    "totalCount": { 
-                        $sum: "$counts" 
-                    }
-                }
-            },
-            {
-                $match: {
-                    $and: [
-                        { "totalCount": { $gt: minCount } },
-                        { "totalCount": { $lt: maxCount } }
-                    ]
-                }
-            },
-            { 
-                $project: { 
-                    "_id": 0, 
-                    "key":1,
-                    "createdAt":1, 
-                    "totalCount": 1
-                }
-            }
-        ];
+    const collectionRecords = client.db(dbName).collection(collectionName);
 
-        const collectionRecords = client.db(dbName).collection(collectionName);
+    return await collectionRecords.aggregate(agg).toArray();
 
-        return await collectionRecords.aggregate(agg).toArray();
-
-        // await recordsCursor.forEach(console.dir);
-        // await mongoDbCloseConnection(client);
-
-    } catch(error) {
-        console.log(error);
-    } finally {
-        await mongoDbCloseConnection(client);
-    }
-}
+    // await recordsCursor.forEach(console.dir);
+    // await mongoDbCloseConnection(client);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await mongoDbCloseConnection(client);
+  }
+};
